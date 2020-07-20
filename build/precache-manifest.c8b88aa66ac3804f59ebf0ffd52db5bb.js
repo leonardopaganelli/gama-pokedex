56119b15abedb5261dc00264d08858d8b0d18d91
Leonardo Paganelli <leonardo.paganelli@iteris.com.br>
2020-07-20 14:11:29 -0300

    add build

---
+++ build/precache-manifest.c8b88aa66ac3804f59ebf0ffd52db5bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab50973dcfe350ea37c939220f9051f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f850457ce2036e503b9d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7fb41d10084ded35b216",
+    "url": "/static/css/main.e5241378.chunk.css"
+  },
+  {
+    "revision": "f850457ce2036e503b9d",
+    "url": "/static/js/2.d1c40ea9.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/static/js/2.d1c40ea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb41d10084ded35b216",
+    "url": "/static/js/main.45063599.chunk.js"
+  },
+  {
+    "revision": "dc104b1a816d25373e6f",
+    "url": "/static/js/runtime-main.9a9dee26.js"
+  },
+  {
+    "revision": "2b4673e318ab94da17bbf9eaad5b80d6",
+    "url": "/static/media/icon-pokeball.2b4673e3.png"
+  }
+]);
