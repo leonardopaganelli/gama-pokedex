0cefc3048853153f30b38a7c595fd097968fffb9
Leonardo Paganelli <leonardo.paganelli@iteris.com.br>
2020-07-20 14:13:23 -0300

    build project

---
+++ build/precache-manifest.b5f11c8cf46ee60f27314419bd606370.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f9ce2c98a73c46483c40be38baeb43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83841c04d1f638bf8705",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a4d7768665259917d1c",
+    "url": "/static/js/2.36613a42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.36613a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83841c04d1f638bf8705",
+    "url": "/static/js/main.41c36efc.chunk.js"
+  },
+  {
+    "revision": "dc104b1a816d25373e6f",
+    "url": "/static/js/runtime-main.9a9dee26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
